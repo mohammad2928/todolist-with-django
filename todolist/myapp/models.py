7292c3794529f2a174b5a68e3a446e9cb29e094b
mohammad2928 <zaribar2928@gmail.com>
2021-06-06 01:36:24 +0430

    add form creaed and done button updated and 3 feild added to the model

--- todolist/myapp/models.py
+++ todolist/myapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime 
 
 # Create your models here.
 class Task(models.Model):
@@ -6,6 +7,9 @@ class Task(models.Model):
     def __str__(self):
         return self.name 
         
-        
-    name = models.CharField(max_length=100)
-    priority = models.IntegerField()
+    name = models.CharField(max_length=100) # title 
+    priority = models.IntegerField(default=1) # a priority between 1-10 (1 is low and 10 is very high)
+    creation_date = models.DateField(default=datetime.date.today()) # date of creation task
+    end_date = models.DateField(default=datetime.date.today()) # expected time for ending the task
+    status = models.IntegerField(default=0) # if 1, it has been done and otherwise is 0 (default).
+    description = models.TextField(default="No description") # description about the task
--- todolist/myapp/views.py
+++ todolist/myapp/views.py
@@ -4,21 +4,71 @@ from .models import Task
 
 # Create your views here.
 
-
 def index_form(request):
+    """
+    main page
+    a list of tasks
+    """
     if request.method == "POST":
-        name = request.POST.get("name","")
-        priority = request.POST.get("priority", "")
-        task = Task(name=name, priority=priority)
-        task.save()
-        return redirect("/")
+         # check done form
+        done_id = request.POST.get("done", "")
+        if done_id:
+            task = Task.objects.filter(id=done_id)
+            if task[0].status == 1:
+                task.update(status=0)
+            else:
+                task.update(status=1)
+            return redirect("/")
+
+        # check filter
+        filter = request.POST.get("filter", "")
+        
+        if filter == "end_date":
+            tasks = Task.objects.order_by('end_date')
+            return render(request, 'myapp/index.html', {'tasks':tasks})
+        elif filter == "start_data":
+            tasks = Task.objects.order_by('name')
+            return render(request, 'myapp/index.html', {'tasks':tasks})
+        elif filter == "priority":
+            tasks = Task.objects.order_by('priority')
+            return render(request, 'myapp/index.html', {'tasks':tasks})
+        elif filter == "dones":
+            tasks = Task.objects.filter(status=1)
+            return render(request, 'myapp/index.html', {'tasks':tasks})
+        elif filter == "remains":
+            tasks = Task.objects.filter(status=0)
+            return render(request, 'myapp/index.html', {'tasks':tasks})
+        else:
+            tasks = Task.objects.order_by('id')
+            return render(request, 'myapp/index.html', {'tasks':tasks})
     else:
         tasks = Task.objects.all()
         return render(request, 'myapp/index.html', {'tasks':tasks})
+
 def delete(request, id):
-    task = Task.objects.get(id=id)
+    """
+    remove a task by id
     
+    """
+
+    task = Task.objects.filter(id=id)
     if request.method == "POST":
-        task.delete()
+        task.update(status=1)
         return redirect("/")
     return render(request, "myapp/delete.html")
+
+def add_task(request):
+    """
+    create a task
+    
+    """
+    if request.method == "POST":
+        name = request.POST.get("name","")
+        priority = request.POST.get("priority", "")
+        end_date = request.POST.get("end_date", "")
+        description = request.POST.get("description", "")
+        task = Task(name=name, priority=priority, description=description, end_date=end_date)
+        task.save()
+        return redirect("/")
+    else:
+        return render(request, "myapp/add.html")
